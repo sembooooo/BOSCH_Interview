da6815ad8096baaee0fe8678f4611cdc9a89d50f
Sana Srikar (RBEI/ESP-IS14) <Srikar.Sana@in.bosch.com>
2021-07-17 07:45:47 +0530

    commiting

--- state.c
+++ state.c
@@ -15,7 +15,7 @@ typedef struct {
  *  ON
  *  OFF*
  * 
- * ON- #. Switch will only stay in ON state for 5msec. This unit will reset state to None
+ * ON- #. Switch shall only stay in ON state for 5msec. This unit shall reset state to None
  *          after 5msec. The user need not worry about resetting.
  *      #. Before Requesting OFF to Switch one "must" check if it is possible
  *          using the API IsOFFReqToSwitchPossible().
@@ -50,9 +50,20 @@ boolean IsOnReqToSwitchPossible(void)  /*Public API */
 void Task_5mSec(void)
 {
     /**
-     * This task runs periodically every 5msec.
-     * 1. After 5msec of ON request , the switch should be set to NoRequest
-     * 2. After setting the switch to NoRequest another ON request is allowed only after 20mSec
-     * 3. Only after completion of 20msec i.e., in 25thmsec a new On request should be allowed
-     */
+     * 
+     * 
+     *                                    +----------15msec-------+-5msec--+ 
+     *          +---5msec----+---5msec----+-----------------------+--------+-----------+
+     *          a            b            c                       c'       d            
+     * 
+     *  a. A new On request is recieved.
+     *  b. Task_5msec shall set the request to NoRequest and shall make sure IsOnReqToSwitchPossible()
+     *     returns FALSE.
+     *  c. Task_5msec shall wait for 15mSec. For all the 15mSec duration IsOnReqToSwitchPossible()
+     *     should return FALSE.
+     *  c`. At c` Task_5msec shall make sure IsOnReqToSwitchPossible() returns TRUE hinting
+     *      it is possible switch ON the switch again.
+     *  d. (just a marker in the graph.)
+     * 
+     */      
 }
